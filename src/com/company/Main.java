551b327d74497051806c5a5f9303067d04af93e8
hugo-obrien <hugue@mail.ru>
2015-12-13 23:37:17 +0300

    тест2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         // Ну что, начнем потихоньку
+        // Так, кое-как скачать репозиторий с GitHub удалось
+        // Теперь попробуем его закоммитить
     }
 }
